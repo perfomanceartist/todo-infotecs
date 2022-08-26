2a4b4078da7a9b2b5edef46fc4d05b3569763374
perfomanceartist <s.sorockoletov@ya.ru>
2022-08-26 15:41:05 +0300

    Basic markup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,105 @@
+import React from "react";
 import ToDoPage from './ToDoPage.js';
-import ToDoList from './ToDoList.js';
+import ToDoElement from './ToDoElement.js';
 import './css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <ToDoList />
-      <ToDoPage />
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    let todos = [ { name: "Eat", status:"done", id: 1}, { name:"Sleep", status:"inProcess", id:2}, { name:"Repeat", status:"waiting", id:3}];
+    this.state = { todos: todos, newToDoVal : "", newId: todos.length, currentToDoName:"", currentToDoStatus:"", currentToDoId:0
+    };
+    this.AddNew = this.AddNew.bind(this);
+    this.FormOnChange = this.FormOnChange.bind(this);
+    this.DeleteToDo = this.DeleteToDo.bind(this);
+    this.ClickToDo = this.ClickToDo.bind(this);
+    this.UpdateToDo = this.UpdateToDo.bind(this);
+  }
+  
+
+  FormOnChange (e) {
+    this.setState({newToDoVal : e.target.value});
+  }
+  UpdateToDo(todo) {
+    console.log("UpdateTodo:")
+    console.log(todo);
+    const index = this.state.todos.findIndex(item => todo.id === item.id);
+    let newTodos = [...this.state.todos];
+
+    newTodos[index].name = todo.name;
+    newTodos[index].status = todo.status;
+    this.setState({todos: newTodos  });
+  }
+  DeleteToDo (id) {
+    const list = this.state.todos.filter( (item) => item.id !== id );
+    console.log(list);
+    this.setState({todos: list});
+  }
+  AddNew (e) {
+    
+    e.preventDefault();
+    let val = this.state.newToDoVal;
+    if (val.length === 0) return;
+    
+    this.setState(state => {
+      let list = [...state.todos, {name:val, status:"inProcess", id: state.newId + 1}];
+      return { todos:list, newToDoVal: "" , newId : state.newId + 1};
+    });
+  }
+  ClickToDo(id, name, status) {
+    console.log({
+      currentToDoId: id,
+      currentToDoName: name,
+      currentToDoStatus: status
+    });
+    this.setState({
+      currentToDoId: id,
+      currentToDoName: name,
+      currentToDoStatus: status
+    });
+  }
+  render () {
+    const todoList = this.state.todos.map( (item) => 
+      
+        <ToDoElement onClick={this.ClickToDo} key={item.id} id={item.id} name={item.name} status={item.status} callback={this.DeleteToDo}/>
+      
+    );
+      return (
+        <div className="App">
+          <div class="todo-list-div">
+            <div class="todo-list-name">
+                Задачи
+            </div>
+            <div class="todo-list">
+                
+                {todoList}
+            </div>
+            <div className="todo-add-form-div">
+            <form className="todo-add-form" onSubmit={this.AddNew}>
+            <input className="todo-add-input" type="text" onChange={this.FormOnChange} value={this.state.newToDoVal} placeholder="Новая задача"/>
+            <div className="todo-add-btn-div">
+              <input className="todo-add-btn" type="submit" value="Добавить"/>
+            </div>
+            </form>
+            </div>
+            </div>
+
+            <ToDoPage key={this.state.currentToDoId} 
+                name={this.state.currentToDoName} 
+                status={this.state.currentToDoStatus} 
+                id={this.state.currentToDoId}
+                updateCallback={this.UpdateToDo}
+                deleteCallback={this.DeleteToDo}
+            />
+        </div>
+      
+    );
+  }
+  
+};
+
+
+
+
 
 export default App;
---
+++ src/ToDoElement.js
@@ -0,0 +1,39 @@
+import React from "react"
+import "./css/ToDoElement.css"
+class ToDoElement extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {name: this.props.name, status:this.props.status, id: this.props.id, deleteCallback: this.props.callback};
+    }
+
+    StatusToClassName = () => {
+        switch (this.props.status) {
+            case "inProcess":
+                return "todo-name todo-inProcess";
+            case "done":
+                return "todo-name todo-done";
+            case "waiting":
+                return "todo-name todo-waiting";
+            default:
+                return "error";
+        }
+    };
+    
+    Delete = () => {
+        this.state.deleteCallback(this.props.id);
+        //this.setState({name: this.props.name, status:this.props.status, id: this.props.id})
+    };
+
+    render () {
+    return (
+        <div className="todo-div" 
+            onClick={ () => {this.props.onClick(this.props.id, this.props.name, this.props.status);} } >
+            <div className={this.StatusToClassName()} > {this.props.name} </div>
+        </div>
+    );
+    }
+   
+}
+ToDoElement.defaultProps = {name:"default name", status:"inProcess"};
+
+export default ToDoElement;
